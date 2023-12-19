fca0ec43c47c87321029cdf4f77298ce165cd52d
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-12-19 22:18:03 +0500

    adding authentication

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
